91a3f21f0a384c0d29c800e1b9ce917ffd6579c3
Francisco <franciscocolombocouste@gmail.com>
2024-06-12 22:10:33 +0200

    Add event listener to button and move functions inside so they execute when button is clicked

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 const container = document.querySelector(".container");
 
-function createDivs () {
-    for(let i = 0; i < 256; i++) {
-        const newDiv = document.createElement("div");
-        container.appendChild(newDiv);
-    }
-}
-
-createDivs();
-
-// Check that the number of divs created is indeed 256
-let numb = container.children.length;
-console.log(numb);
-
 // set a class to all child divs of the parent div "container"
 
 const childDivCollection = container.children;
@@ -23,31 +10,37 @@ function setClassOfChildDiv () {
     }
 }
 
-setClassOfChildDiv ();
-
-for (let i = 0; i < childDivCollection.length; i++) {
-    childDivCollection[i].addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor= "rgb(211, 211, 211)";
-    });
-}
-
 const body = document.querySelector("body")
 const btn = document.createElement("button");
 body.appendChild(btn);
 
-btn.textContent="Choose Grid Size"
+btn.textContent="Choose Grid Size";
 
 function getUserInput () {
-    let userInput = prompt("Please enter the desired grid size (eg. '64' for a 64x64 grid)", "16");
+    let userInput= prompt("Please enter the desired grid size (eg. '64' for a 64x64 grid)", "16");
     return userInput;
 };
 
 btn.addEventListener("click", () => {
-    userInput = getUserInput ();
+    // existing grid is removed here by using the replaceChildren() method
+     container.replaceChildren();
+     let gridSize = getUserInput ();
+     function createDivs (gridSize) {
+        for(let i = 0; i < gridSize * gridSize; i++) {
+            const newDiv = document.createElement("div");
+            container.appendChild(newDiv);
+        }
+    }
+    createDivs(gridSize);
+    setClassOfChildDiv ();
+    for (let i = 0; i < childDivCollection.length; i++) {
+        childDivCollection[i].addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor= "rgb(211, 211, 211)";
+        });
+    }
+// set width and height of child div based on chosen grid size
+    for (let i = 0; i < childDivCollection.length; i++) {
+        childDivCollection[i].style.width=`${100/gridSize}%`;
+        childDivCollection[i].style.height=`${100/gridSize}%`;
+    }
 });
-
-// STEPS to complete STEP 4 of project
-// 1. User clicks button
-// 2. prompt asks user for their desired grid size (eg. "64" for 64x64 grid)
-// 3. existing grid is removed
-// 4. new grid is generated (in the same total space as before so that you have a new sketchpad)
